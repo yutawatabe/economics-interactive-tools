8078cfa6c63161d110509b132be28ab20fda2fbe
yutawatabe <37814684+yutawatabe@users.noreply.github.com>
2025-04-28 18:19:49 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// This file is currently empty as the main page is static.
+// You can add JavaScript here later if you want to add features like:
+// - Dynamic filtering of the application list
+// - Animations or transitions
+// - Loading content dynamically
+// - etc.
+// For now, it serves as a placeholder.
